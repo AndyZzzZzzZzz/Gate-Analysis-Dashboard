31d5cfa80210c243b2b8b41abb9ab872eacaff7f
AndyZhang <zkpAndy@outlook.com>
2025-07-27 16:16:32 -0700

    Add example for api and data preparation

--- backend/api/data.py
+++ backend/api/data.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
-from services.excel_service import get_char_data
+from fastapi import Query
+from services.excel_services import *
 
 router = APIRouter()
 
-@router.get("/data")
-async def get_data():
-    data = get_chart_data()
+@router.get("/data/get_worst_subjects_data")
+async def get_worst_subjects(subject: str = Query(..., example="STAT")):
+    data = get_worst_subjects_data(subject)
     return data
 
 
--- backend/services/excel_services.py
+++ backend/services/excel_services.py
@@ -1,10 +1,47 @@
 import pandas as pd
+import os 
 
-def get_chart_data():
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+EXCEL_PATH = os.path.join(BASE_DIR, '..', 'data', 'gatekeeper_d_cutoff_202223.xlsx')
+EXCEL_PATH = os.path.abspath(EXCEL_PATH)
 
-    return {
-        "labels": ["A", "B", "C"],
-        "values": [10, 11, 12],
-    }
+# read in data and parse sheets
+# all_hseet = {sheet_name: dataFrame ..}
+excel_file = pd.ExcelFile (EXCEL_PATH)
+all_sheets = pd.read_excel(excel_file, sheet_name = None)
+raw_data = all_sheets['Raw Grade Data']
+
+
+
+
+def get_worst_subjects_data(subject: str) -> dict:
+    """
+        Fetch the top 10 worst performing courses within a given subject
+        The courses are ranked with their highest rates of "D or less" performance
+    """
+    subject_data = raw_data[raw_data['Course Subject'] == subject]
+
+    subject_data['Fail Rate'] = (subject_data['# D'] + subject_data['# F'])/subject_data['Total #']
+    top10 = subject_data.sort_values('Fail Rate', ascending=False).head(10)
     
+    failrate_dict = {}
+    failrate_dict['Others'] = 1
+    for _, row in top10.iterrows():
+        course = row['Course Subject'] + row['Catalog Number']
+        failrate = row['Fail Rate']
+        failrate_dict[course] = failrate
+        failrate_dict['Others'] -= failrate
+    
+    return failrate_dict
+
+
+def get_faculty_data(faculty: str):
+
+    pass
+
+def get_course_data(course: str):
+    pass
+
+def get_population_data():
+    pass
     
