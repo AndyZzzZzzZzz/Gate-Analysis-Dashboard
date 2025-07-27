ea9a459019290ad19c6d52da0c01b46f5f4356fd
AndyZhang <zkpAndy@outlook.com>
2025-07-27 16:21:54 -0700

    Update date and requirements

--- backend/api/data.py
+++ backend/api/data.py
@@ -4,6 +4,8 @@ from services.excel_services import *
 
 router = APIRouter()
 
+# curl "http://localhost:8000/api/data/get_worst_subjects_data?subject=STAT"
+
 @router.get("/data/get_worst_subjects_data")
 async def get_worst_subjects(subject: str = Query(..., example="STAT")):
     data = get_worst_subjects_data(subject)
