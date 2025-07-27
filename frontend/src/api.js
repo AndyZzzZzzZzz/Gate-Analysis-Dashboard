d59abab06897b9264e6b749e02270e14bb3fcf7f
AndyZhang <zkpAndy@outlook.com>
2025-07-27 16:59:45 -0700

    Update api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
-// Fetch data from Pie chartw
-export async function fetchChartData() {
-  const res = await fetch('http://localhost:8000/api/data');
-  if (!res.ok) throw new Error("Failed to fetch chart data");
+// Fetch data from Pie chart
+export async function fetchSubjectData() {
+  const res = await fetch('http://localhost:9000/api/data/get_worst_subjects_data?subject=STAT');
+  if (!res.ok) throw new Error("Failed to fetch subjects data");
   return await res.json();
 }
